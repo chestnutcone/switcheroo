961289da419ed20deda7c2997e0a64c46868228d
Oliver <oliver37215@hotmail.com>
2019-10-24 22:37:22 -0700

    Update settings.py

--- switcheroo/settings.py
+++ switcheroo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'z@2rj6!(3!8hzm5!@j-zhpy9sgoubo-ix*fiz&!ly95emm=d+%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'False'
