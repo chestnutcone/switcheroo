14eb845e4464657cc168b297cdc5a21c134f10a9
Oliver <oliver37215@hotmail.com>
2019-10-30 14:32:02 -0700

    push to heroku

--- switcheroo/settings.py
+++ switcheroo/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [".herokuapp.com", "oliverswitcheroo.herokuapp.com", "127.0.0.1", "localhost"]
 
