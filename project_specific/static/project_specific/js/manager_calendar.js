f139fcf1691cb52a2420e36cbf8c4410aa267042
Oliver <oliver37215@hotmail.com>
2019-10-30 23:52:07 -0700

    changed navbar

--- project_specific/static/project_specific/js/manager_calendar.js
+++ project_specific/static/project_specific/js/manager_calendar.js
@@ -127,7 +127,7 @@ function checkEvent () {
     let daily_schedules = monthSchedules[selected_dates]
     let daily_summary = document.getElementById('today-summary')
     let title = document.createElement('h1')
-    title.innerText = 'Dai Summary'
+    title.innerText = 'Daily Summary'
     daily_summary.appendChild(title)
 
     for (p in daily_schedules) {
